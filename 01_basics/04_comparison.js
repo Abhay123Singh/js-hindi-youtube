7119a5040555f7e774e812ab1c792b63a14d4a3d
Abhay123Singh <146925664+Abhay123Singh@users.noreply.github.com>
2026-03-15 04:41:40 +0000

    com[parison.js

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,12 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// we avoid this type of conversion because we want our code are clean and easy to  understand.
+//  === 
+console.log("2" === 2);
