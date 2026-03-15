1b211e708fbdabe40e395e6fcc9adf5fa46844d0
Abhay123Singh <146925664+Abhay123Singh@users.noreply.github.com>
2026-03-15 04:07:26 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,12 +23,46 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
+// console.log(stringNumber);
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+//************************************operation***************** */
+
+let value = 3
+let negative = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+
+let str1 = "hello"
+let str2 = "hitesh"
+
+let str3  = str1 + str2
+console.log(str3);
 
 
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1+2+"2");
 
+// console.log((3+4)*5%3);
+
+console.log(+true);
+console.log(+"");
+
+let num1 , num2 , num3
+
+num1  = num = num3 = 2+2
+
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+// in this we have to focus on post/pre increment operation
+// told by sir
 
 
 
